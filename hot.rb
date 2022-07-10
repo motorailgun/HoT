4d33fc9e95a7fa828db51c0f14553bd114e0122b
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-07-11 07:30:58 +0900

    HoT: initial revision

---
+++ hot.rb
@@ -0,0 +1,11 @@
+puts "Tossing a coin..."
+
+results = (0..2).to_a.reduce([0, 0]){|result, count|
+	is_head = Kernel::rand(0..1) == 0
+	puts "Round #{count + 1}: #{is_head ? 'Heads' : 'Tails'}"
+	
+	result[is_head ? 0 : 1] += 1
+	result
+}
+
+puts "Heads: #{results[0]}, Tails: #{results[1]}"
